3c912fbec926c043a38f265da850da60676de939
ayush <axlee484@users.noreply.github.com>
2026-01-19 02:50:38 +0530

    update export

--- dist/web/index.service.worker.js
+++ dist/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768771079|16175791406';
+const CACHE_VERSION = '1768771219|16315694901';
 /** @type {string} */
 const CACHE_PREFIX = 'sokoban-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
