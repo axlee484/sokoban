b9a8438ca27a28cfcdb1de49c9bafac465956f4d
ayush <axlee484@users.noreply.github.com>
2026-01-18 16:26:16 +0530

    base setup

---
+++ singletons/gameData.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var LEVEL_DATA_PATH: String = "res://data/level_data.json"
+var tileSize: int = 32
+
+var levelData: Dictionary = {}
+
+
+
+
+
+func loadLevelData():
+	var file = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
+	levelData = JSON.parse_string(file.get_as_text())
+
+
+func getLevelData(level: int):
+	return levelData.get(str(level))
+
+
+func _ready():
+	loadLevelData()
